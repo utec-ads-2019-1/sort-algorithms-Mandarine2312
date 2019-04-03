4914637f45a3bd23572abcc1ed5f76e9d576b253
Alejandro <alejandro.tang@utec.edu.pe>
2019-04-02 21:18:48 -0500

    Select Sort v1

--- select.h
+++ select.h
@@ -8,7 +8,18 @@ class SelectSort : public Sort {
         SelectSort(void *elements, size_t size) : Sort(elements, size) {}
 
         void execute(void (*compare)(void*, int, int)) {
-            // TODO
+            int *positionOfMax = new int;
+            int *array = (int*) elements;
+
+            for(int i = size; i >= 0; i--){
+                *positionOfMax = i;
+                for(int j = i; j >= 0; j--){
+                    if(array[j] > array[*positionOfMax]){
+                        *positionOfMax = j;
+                    }
+                }
+                swap(array[i], array[*positionOfMax]);
+            }
         }
 
         inline string name() { return "SelectSort"; }
