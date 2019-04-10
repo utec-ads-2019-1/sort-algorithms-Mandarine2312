87670ff009a5c69c87b0503397e7d6dc6e6b3c77
alejandro <alejandro.tang@utec.edu.pe>
2019-04-09 21:39:54 -0500

    por acabar

--- shell.h
+++ shell.h
@@ -8,14 +8,9 @@ class ShellSort : public Sort {
         ShellSort(void *elements, size_t size) : Sort(elements, size) {}
 
         void execute(void (*compare)(void*, int, int)) {
-            for(int i = (size/2)+1; i >= 1; i--){
+            for(int i = (size/2); i >= 1; i--){
                 for(int j = 0; j < size - i; j++){
                     compare(elements, j, j+i);}
-            }
-            for(int i = 1; i < size; i++){
-                for(int j = i; j >0; j--) {
-                    compare(elements, j-1, j);
-                }
             }
         }
 
@@ -31,5 +26,4 @@ class ShellSort : public Sort {
 
         inline string name() { return "ShellSort"; }
 };
-
 #endif
